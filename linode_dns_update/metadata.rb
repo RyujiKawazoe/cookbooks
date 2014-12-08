8c474ea9a0ee53de465648c31290cf07b82003da
Isao SHIMIZU <isaoshimizu@gmail.com>
2014-12-08 16:47:36 +0900

    Add name in metadata.rb, bump version

--- linode_dns_update/metadata.rb
+++ linode_dns_update/metadata.rb
@@ -2,6 +2,7 @@ maintainer "James Harton, Sociable Limited."
 maintainer_email "james@sociable.co.nz"
 license "MIT"
 description "Updates DNS on linode.com with the machines IP address and hostname."
-version "0.0.1"
+version "0.0.2"
+name    "linode_dns_update"
 
 recipe "linode_dns_update", "Update linode DNS."
--- runit/metadata.rb
+++ runit/metadata.rb
@@ -2,7 +2,8 @@ maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs runit and provides runit_service definition"
-version           "0.14.2"
+version           "0.14.3"
+name              "runit"
 
 recipe "runit", "Installs and configures runit"
 
